88d45348872c3a90a9bcf14c2a781f400dcc759f
KienVu <kienidkcgo1001@gmail.com>
2025-02-22 22:17:57 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunForLive.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
